2e43a8cce149ef6b15d27aa1dbafd4c03d989f5d
Oleksandr Fediashov <olfedias@microsoft.com>
2022-01-18 16:59:30 +0100

    add comments to Rollup changes

--- tools/getRollupOptions.js
+++ tools/getRollupOptions.js
@@ -1,4 +1,8 @@
 function getRollupOptions(/** @type {import('rollup').RollupOptions} */ options) {
+  // Following options are overridden:
+  // - `preserveModules` & `preserveModulesRoot` to stop bundling to a single file and prevent bundle size issues
+  // - `interop` to not include interop for `import * as` syntax
+
   if (Array.isArray(options.output)) {
     options.output.forEach(o => {
       o.preserveModules = true;
