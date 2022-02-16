a8b0853d9db25a6835e44e535f8ad3b35f0c502d
Oleksandr Fediashov <olfedias@microsoft.com>
2022-02-16 16:29:18 +0100

    chore: add tests for Vite plugin

---
+++ packages/vite-plugin/__fixtures__/object/output.ts
@@ -0,0 +1,12 @@
+import { __styles } from '@griffel/react';
+const styles = /* @__PURE__ */ __styles(
+  {
+    root: {
+      sj55zd: 'fl9q5hc',
+    },
+  },
+  {
+    d: ['.fl9q5hc{color:var(--colorBrandStroke1);}'],
+  },
+);
+console.log(styles);
