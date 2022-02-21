26f1bcb621ced6ef7ca37278a1ff93bf005df950
Oleksandr Fediashov <olfedias@microsoft.com>
2022-02-21 17:52:49 +0100

    fix: enable interop back in Rollup

--- tools/getRollupOptions.js
+++ tools/getRollupOptions.js
@@ -1,13 +1,12 @@
 function getRollupOptions(/** @type {import('rollup').RollupOptions} */ options) {
   // Following options are overridden:
   // - `preserveModules` & `preserveModulesRoot` to stop bundling to a single file and prevent bundle size issues
-  // - `interop` to not include interop for `import * as` syntax
+  // - `sourcemap` to enable sourcemaps
 
   if (Array.isArray(options.output)) {
     options.output.forEach(o => {
       o.preserveModules = true;
       o.preserveModulesRoot = 'src';
-      o.interop = false;
       o.sourcemap = true;
     });
   } else {
@@ -15,7 +14,6 @@ function getRollupOptions(/** @type {import('rollup').RollupOptions} */ options)
       ...options.output,
       preserveModules: true,
       preserveModulesRoot: 'src',
-      interop: false,
       sourcemap: true,
     };
   }
