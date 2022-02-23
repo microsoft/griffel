e46cd4491118473382ceb0bf59e461c1d875f741
Miroslav Stastny <miroslav.stastny@microsoft.com>
2022-02-23 13:27:12 +0100

    chore(storybook): Use @babel/typescript

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,7 @@ module.exports = {
   babel: async options => {
     return {
       ...options,
-      presets: [...options.presets, '@nrwl/react/babel'],
+      presets: [...options.presets, '@babel/typescript'],
     };
   },
 };
