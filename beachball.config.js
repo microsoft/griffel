ea4b3cec842735307238032c6535e33d98d2f26a
Oleksandr Fediashov <olfedias@microsoft.com>
2022-03-08 14:08:56 +0100

    chore: fix publishing with beachball

--- beachball.config.js
+++ beachball.config.js
@@ -1,8 +1,7 @@
-// @ts-check
-
 /**
  * @type {import('beachball').BeachballConfig}
  */
 module.exports = {
   gitTags: false,
+  hooks: require('./beachball.hooks'),
 };
---
+++ beachball.hooks.js
@@ -0,0 +1,53 @@
+// @ts-check
+
+const childProcess = require('child_process');
+
+/**
+ * @param {String} command
+ *
+ * @returns {Promise<string>}
+ */
+function sh(command) {
+  return new Promise((resolve, reject) => {
+    const [cmd, ...args] = command.split(' ');
+
+    /** @type {import('child_process').SpawnOptions} */
+    const options = {
+      cwd: process.cwd(),
+      env: process.env,
+      stdio: 'inherit',
+      shell: true,
+    };
+    const child = childProcess.spawn(cmd, args, options);
+
+    let stdoutData = '';
+
+    if (child.stdout) {
+      child.stdout.on('data', data => {
+        stdoutData += data;
+      });
+    }
+
+    child.on('close', code => {
+      if (code === 0) {
+        resolve(stdoutData);
+      }
+
+      reject(new Error([`child process exited with code ${code}`, stdoutData].join('\n')));
+    });
+  });
+}
+
+let completedPostBump = false;
+
+/**
+ * @type {import('beachball').BeachballConfig['hooks']}
+ */
+module.exports = {
+  async prepublish() {
+    if (!completedPostBump) {
+      await sh('yarn build --skip-nx-cache');
+      completedPostBump = true;
+    }
+  },
+};
