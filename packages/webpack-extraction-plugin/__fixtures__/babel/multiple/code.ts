23634d0e943fe972c70251cb76c2bf25ee92a6ff
Oleksandr Fediashov <olfedias@microsoft.com>
2022-03-21 12:49:29 +0100

    feat: add Babel plugin for CSS extraction

---
+++ packages/webpack-extraction-plugin/__fixtures__/babel/multiple/code.ts
@@ -0,0 +1,23 @@
+import { __styles } from '@griffel/react';
+
+export const stylesA = __styles(
+  {
+    root: {
+      sj55zd: 'fe3e8s9',
+    },
+  },
+  {
+    d: ['.fe3e8s9{color:red;}'],
+  },
+);
+
+export const stylesB = __styles(
+  {
+    root: {
+      De3pzq: 'fcnqdeg',
+    },
+  },
+  {
+    d: ['.fcnqdeg{background-color:green;}'],
+  },
+);
---
+++ packages/webpack-extraction-plugin/src/babelPluginStripGriffelRuntime.test.ts
@@ -0,0 +1,36 @@
+import pluginTester, { prettierFormatter } from 'babel-plugin-tester';
+import * as fs from 'fs';
+import * as path from 'path';
+
+import { babelPluginStripGriffelRuntime } from './babelPluginStripGriffelRuntime';
+
+const prettierConfig = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '../../../.prettierrc'), { encoding: 'utf-8' }),
+);
+const fixturesDir = path.join(__dirname, '..', '__fixtures__', 'babel');
+
+pluginTester({
+  babelOptions: {
+    parserOpts: {
+      plugins: ['typescript'],
+    },
+  },
+  pluginOptions: {
+    babelOptions: {
+      presets: ['@babel/typescript'],
+    },
+  },
+  formatResult: code =>
+    prettierFormatter(code, {
+      config: {
+        ...prettierConfig,
+        parser: 'typescript',
+      },
+    }),
+
+  fixtures: fixturesDir,
+  tests: [],
+
+  plugin: babelPluginStripGriffelRuntime,
+  pluginName: '@griffel/webpack-extraction-plugin/babel',
+});
