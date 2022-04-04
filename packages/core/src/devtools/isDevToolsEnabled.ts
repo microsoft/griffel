dceba5da309a87d396717c05838857c4bf7f4d06
YuanboXue-Amber <yuanboxue@microsoft.com>
2022-04-04 11:13:46 +0200

    remove warnings

--- packages/core/src/devtools/isDevToolsEnabled.ts
+++ packages/core/src/devtools/isDevToolsEnabled.ts
@@ -1,35 +1,13 @@
 const isEnabled = () => {
-  let enabled = false;
+  let griffelDevtoolsEnabled = false;
   if (process.env.NODE_ENV !== 'production') {
     try {
-      const griffelDevtoolsEnabled = !!window.localStorage['griffelDebug'];
-
-      if (process.env.NODE_ENV !== 'test') {
-        if (griffelDevtoolsEnabled) {
-          console.warn(
-            [
-              '@griffel/core:',
-              `devtools are enabled.`,
-              'To remove this override paste `delete window.localStorage.griffelDebug` to your browser console and reload the page.',
-            ].join(' '),
-          );
-        } else {
-          console.warn(
-            [
-              '@griffel/core:',
-              `devtools are disabled.`,
-              'To enable devtools paste `window.localStorage.griffelDebug = true` to your browser console and reload the page.',
-            ].join(' '),
-          );
-        }
-      }
-
-      enabled = griffelDevtoolsEnabled;
+      griffelDevtoolsEnabled = !!window.localStorage['griffelDebug'];
       // eslint-disable-next-line no-empty
     } catch {}
   }
 
-  return enabled;
+  return griffelDevtoolsEnabled;
 };
 
 export const isDevToolsEnabled = isEnabled();
