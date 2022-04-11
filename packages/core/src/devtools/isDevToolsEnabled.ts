c17e292857a9fb6af08ae37299605c6e00715d4a
YuanboXue-Amber <yuanboxue@microsoft.com>
2022-04-11 15:10:29 +0200

    temporarily use isDevToolsEnabled = false until we have extension

--- packages/core/src/devtools/isDevToolsEnabled.ts
+++ packages/core/src/devtools/isDevToolsEnabled.ts
@@ -1,13 +1,1 @@
-const isEnabled = () => {
-  let griffelDevtoolsEnabled = false;
-  if (process.env.NODE_ENV !== 'production') {
-    try {
-      griffelDevtoolsEnabled = !!window.localStorage['griffelDebug'];
-      // eslint-disable-next-line no-empty
-    } catch {}
-  }
-
-  return griffelDevtoolsEnabled;
-};
-
-export const isDevToolsEnabled = isEnabled();
+export const isDevToolsEnabled = false;
