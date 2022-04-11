6a0f6e24af80afedf7b47c73f72c71a439b43aca
Oleksandr Fediashov <olfedias@microsoft.com>
2022-04-11 10:10:34 +0200

    chore: integrate bundle size tooling (#87)
    * chore: integrate bundle size tooling
    
    * fix change files

--- monosize.config.js
+++ monosize.config.js
@@ -1,6 +1,12 @@
 const path = require('path');
 
 module.exports = {
+  repository: 'https://github.com/microsoft/griffel',
+  storage: require('monosize-storage-upstash')({
+    url: 'https://us1-nearby-hyena-35747.upstash.io',
+    readonlyToken:
+      'AoujASQgNGQwNGI0YjAtNmExNy00MzNjLWIwNTQtMTIyMGRlODQwZDk2QaCJNVtEfHj6pDLLscOcgMEt-Be1VWcMYbMRR_EqVCw=',
+  }),
   webpack: config => {
     return {
       ...config,
