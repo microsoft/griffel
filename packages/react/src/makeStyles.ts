2391de2b83ac11647fda47d71c6843df90890d25
Oleksandr Fediashov <olfedias@microsoft.com>
2022-04-20 10:04:33 +0200

    fix: avoid errors with React 18 (#95)

--- packages/react/src/makeStyles.ts
+++ packages/react/src/makeStyles.ts
@@ -6,10 +6,13 @@ import { useRenderer } from './RendererContext';
 import { useTextDirection } from './TextDirectionContext';
 
 function isInsideComponent() {
+  // React 18 always logs errors if a dispatcher is not present:
+  // https://github.com/facebook/react/blob/42f15b324f50d0fd98322c21646ac3013e30344a/packages/react/src/ReactHooks.js#L26-L36
+  //
+  // A check with hooks call (i.e. call "React.useContext()" outside a component) will always produce errors
   try {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    React.useContext({} as unknown as React.Context<unknown>);
-    return true;
+    // @ts-expect-error "SECRET_INTERNALS" are not typed
+    return !!React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentDispatcher.current;
   } catch (e) {
     return false;
   }
