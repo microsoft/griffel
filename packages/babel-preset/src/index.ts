7cbdfc73945ae85da7b0d35a1fa808559a9aa5af
Oleksandr Fediashov <olfedias@microsoft.com>
2022-04-21 10:33:50 +0200

    chore: export "EvalCache" & "Module" from @griffel/babel-preset (#94)

--- packages/babel-preset/src/index.ts
+++ packages/babel-preset/src/index.ts
@@ -4,6 +4,7 @@ import type { ConfigAPI } from '@babel/core';
 import type { BabelPluginOptions } from './types';
 
 export { default as shakerEvaluator } from '@linaria/shaker';
+export { EvalCache, Module } from '@linaria/babel-preset';
 export { configSchema } from './schema';
 
 export type { Evaluator, EvalRule } from '@linaria/babel-preset';
