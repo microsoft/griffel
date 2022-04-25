7f211121075266d45e6c38e009ab9f4caf0016d3
Oleksandr Fediashov <olfedias@microsoft.com>
2022-04-25 11:31:41 +0200

    minor changes

--- packages/babel-preset/__fixtures__/duplicated-imports/code.ts
+++ packages/babel-preset/__fixtures__/duplicated-imports/code.ts
@@ -1,24 +1,12 @@
 // @ts-expect-error This module does not exist, but will be resolved via aliases
 import { createStylesA } from 'custom-package';
 // @ts-expect-error This module does not exist, but will be resolved via aliases
-import { createStyles } from 'custom-package';
-// @ts-expect-error This module does not exist, but will be resolved via aliases
-import { createStylesB } from 'another-custom-package';
-// @ts-expect-error This module does not exist, but will be resolved via aliases
-import { createStylesC } from 'another-package';
+import { createStylesB } from 'custom-package';
 
-export const useStyles = createStyles({
+export const useClassesA = createStylesA({
   root: { color: 'red' },
 });
 
-export const useClassesA = createStylesA({
-  root: { color: 'yellow' },
-});
-
 export const useClassesB = createStylesB({
-  root: { color: 'blue' },
-});
-
-export const useClassesC = createStylesC({
-  root: { color: 'orange' },
+  root: { color: 'yellow' },
 });
--- packages/babel-preset/__fixtures__/duplicated-imports/output.ts
+++ packages/babel-preset/__fixtures__/duplicated-imports/output.ts
@@ -1,12 +1,8 @@
 // @ts-expect-error This module does not exist, but will be resolved via aliases
-import { createStylesA } from 'custom-package'; // @ts-expect-error This module does not exist, but will be resolved via aliases
-
 import { __styles } from 'custom-package'; // @ts-expect-error This module does not exist, but will be resolved via aliases
 
-import { createStylesB } from 'another-custom-package'; // @ts-expect-error This module does not exist, but will be resolved via aliases
-
-import { createStylesC } from 'another-package';
-export const useStyles = __styles(
+import { createStylesB } from 'custom-package';
+export const useClassesA = __styles(
   {
     root: {
       sj55zd: 'fe3e8s9',
@@ -16,18 +12,8 @@ export const useStyles = __styles(
     d: ['.fe3e8s9{color:red;}'],
   },
 );
-export const useClassesA = createStylesA({
-  root: {
-    color: 'yellow',
-  },
-});
 export const useClassesB = createStylesB({
   root: {
-    color: 'blue',
-  },
-});
-export const useClassesC = createStylesC({
-  root: {
-    color: 'orange',
+    color: 'yellow',
   },
 });
