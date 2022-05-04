6826a6463de3530f645fec018c641b744d3ff0ff
Oleksandr Fediashov <alexander.mcgarret@gmail.com>
2022-05-04 17:57:07 +0200

    Update packages/react/src/makeStyles.ts
    Co-authored-by: ling1726 <lingfangao@hotmail.com>

--- packages/react/src/makeStyles.ts
+++ packages/react/src/makeStyles.ts
@@ -13,7 +13,7 @@ function isInsideComponent() {
     const dispatcher = React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentDispatcher.current;
 
     // Before any React component was rendered "dispatcher" will be "null"
-    if (dispatcher === null) {
+    if (dispatcher === null || dispatcher === undefined) {
       return false;
     }
 
