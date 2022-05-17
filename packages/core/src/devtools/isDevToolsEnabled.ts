8195cb1a6c476c464b677be0def840b211d49793
Oleksandr Fediashov <olfedias@microsoft.com>
2022-05-17 10:05:11 +0200

    fix: update isDevToolsEnabled check

--- packages/core/src/devtools/isDevToolsEnabled.ts
+++ packages/core/src/devtools/isDevToolsEnabled.ts
@@ -1,3 +1,9 @@
-export const isDevToolsEnabled = Boolean(
-  typeof window !== 'undefined' && window?.sessionStorage?.getItem('__GRIFFEL_DEVTOOLS__'),
-);
+export const isDevToolsEnabled: Boolean = (() => {
+  // Accessing "window.sessionStorage" causes an exception when third party cookies are blocked
+  // https://stackoverflow.com/questions/30481516/iframe-in-chrome-error-failed-to-read-localstorage-from-window-access-deni
+  try {
+    return Boolean(typeof window !== 'undefined' && window?.sessionStorage?.getItem('__GRIFFEL_DEVTOOLS__'));
+  } catch (e) {
+    return false;
+  }
+})();
