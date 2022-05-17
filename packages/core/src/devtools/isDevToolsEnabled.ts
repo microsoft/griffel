4cc30424fb5978b7893a1b3d75d07c1f642b8fbd
Oleksandr Fediashov <olfedias@microsoft.com>
2022-05-17 10:14:56 +0200

    fix type, remove optional chaining

--- packages/core/src/devtools/isDevToolsEnabled.ts
+++ packages/core/src/devtools/isDevToolsEnabled.ts
@@ -1,8 +1,8 @@
-export const isDevToolsEnabled: Boolean = (() => {
+export const isDevToolsEnabled: boolean = (() => {
   // Accessing "window.sessionStorage" causes an exception when third party cookies are blocked
   // https://stackoverflow.com/questions/30481516/iframe-in-chrome-error-failed-to-read-localstorage-from-window-access-deni
   try {
-    return Boolean(typeof window !== 'undefined' && window?.sessionStorage?.getItem('__GRIFFEL_DEVTOOLS__'));
+    return Boolean(typeof window !== 'undefined' && window.sessionStorage?.getItem('__GRIFFEL_DEVTOOLS__'));
   } catch (e) {
     return false;
   }
