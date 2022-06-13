ff1ff5f51fad1effe93b718393584c8df4978cbf
Lingfan Gao <lingfangao@hotmail.com>
2022-06-13 16:51:00 +0200

    remove benchmark

--- apps/benchmark/src/components/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as Select } from './Select';
-export { default as ReportCard } from './ReportCard';
--- apps/benchmark/src/implementations/inline-styles/Provider.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const InlineStylesProvider: React.FC = ({ children }) => {
-  return <>{children}</>;
-};
-
-export default InlineStylesProvider;
