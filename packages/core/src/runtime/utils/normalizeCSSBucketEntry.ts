d15dbda2e8f27c5872bfd232feffcbf81dd63643
Lingfan Gao <lingfangao@hotmail.com>
2022-06-17 10:01:11 +0200

    test normalizeCSSBucketEntry

--- packages/core/src/runtime/utils/normalizeCSSBucketEntry.ts
+++ packages/core/src/runtime/utils/normalizeCSSBucketEntry.ts
@@ -1,8 +1,18 @@
 import { CSSBucketEntry } from '../../types';
 
+/**
+ * @param entry - CSS bucket entry that can be either a string or an array
+ * @returns An array where the first element is the CSS rule
+ */
 export function normalizeCSSBucketEntry(entry: CSSBucketEntry): [string] | [string, Record<string, unknown>] {
   if (!Array.isArray(entry)) {
     return [entry];
+  }
+
+  if (process.env.NODE_ENV !== 'production' && entry.length > 2) {
+    throw new Error(
+      'CSS Bucket contains an entry with greater than 2 items, please report this to https://github.com/microsoft/griffel/issues',
+    );
   }
 
   return entry;
