1335e033f634b7e47ec4265864842e0633da3974
Amber <yuanboxue@outlook.com>
2022-06-29 13:43:00 +0200

    Update packages/devtools/pack-extension.js
    Co-authored-by: Oleksandr Fediashov <alexander.mcgarret@gmail.com>

--- packages/devtools/pack-extension.js
+++ packages/devtools/pack-extension.js
@@ -1,15 +1,37 @@
-// sync manifest version with package.json
-const version = require('./package.json').version;
-const manifestPath = '../../dist/packages/devtools/manifest.json';
-const manifest = require(manifestPath);
-manifest.version = version;
-require('fs').writeFileSync(manifestPath, JSON.stringify(manifest, null, 2));
-
-// pack extension
-// eslint-disable-next-line import/no-extraneous-dependencies
+const fs = require('fs');
+const path = require('path');
 const zip = require('bestzip');
+
+const DIST_PATH = path.resolve(__dirname, '../../dist/packages/devtools');
+
+if (!fs.existsSync(DIST_PATH)) {
+  console.error(`"${DIST_PATH}" does not exist, please run "build" command first`);
+  process.exit(1);
+}
+
+// Syncs the version from "package.json" with "manifest.json"
+const actualVersion = require('./package.json').version;
+
+const manifestPath = path.resolve(DIST_PATH, 'manifest.json');
+const manifestJSON = require(manifestPath);
+
+fs.writeFileSync(
+  manifestPath,
+  JSON.stringify(
+    {
+      ...manifestJSON,
+      version: actualVersion,
+    },
+    null,
+    2,
+  ),
+);
+
+// Pack extension into .zip archive for publishing
+// eslint-disable-next-line import/no-extraneous-dependencies
 zip({
   source: '*',
   destination: '../ext.zip',
-  cwd: '../../dist/packages/devtools/',
+  cwd: DIST_PATH,
 });
+
