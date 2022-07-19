1dcfb09cd987edce0ee7d7f5f4e23bab79597356
Oleksandr Fediashov <olfedias@microsoft.com>
2022-07-19 11:37:15 +0200

    use reduce

--- apps/website/sidebars.js
+++ apps/website/sidebars.js
@@ -5,54 +5,55 @@ const babel = require('@babel/core');
 function generateTryItOutSidebar() {
   const playgroundTemplatePath = path.join(__dirname, '/src/components/Playground/code/templates');
   const templateFiles = fs.readdirSync(playgroundTemplatePath);
-  const sidebarItems = [];
 
-  templateFiles.forEach(templateFile => {
-    const id = path.parse(templateFile).name;
-    const templatePath = path.join(playgroundTemplatePath, templateFile);
+  return templateFiles
+    .reduce((sidebarItems, templateFile) => {
+      const id = path.parse(templateFile).name;
+      const templatePath = path.join(playgroundTemplatePath, templateFile);
 
-    const code = fs.readFileSync(templatePath, { encoding: 'utf-8' });
-    const res = babel.parseSync(code, { filename: templateFile });
-    const meta = { name: id };
+      const code = fs.readFileSync(templatePath, { encoding: 'utf-8' });
+      const res = babel.parseSync(code, { filename: templateFile });
+      const meta = { name: id };
 
-    babel.traverse(res, {
-      VariableDeclarator(path) {
-        const idPath = path.get('id');
-        const initPath = path.get('init');
+      babel.traverse(res, {
+        VariableDeclarator(path) {
+          const idPath = path.get('id');
+          const initPath = path.get('init');
 
-        if (idPath.isIdentifier({ name: 'meta' }) && initPath.isObjectExpression) {
-          const properties = initPath.get('properties');
+          if (idPath.isIdentifier({ name: 'meta' }) && initPath.isObjectExpression) {
+            const properties = initPath.get('properties');
 
-          if (Array.isArray(properties)) {
-            properties.forEach(propertyPath => {
-              if (propertyPath.isObjectProperty()) {
-                const keyPath = propertyPath.get('key');
-                const valuePath = propertyPath.get('value');
+            if (Array.isArray(properties)) {
+              properties.forEach(propertyPath => {
+                if (propertyPath.isObjectProperty()) {
+                  const keyPath = propertyPath.get('key');
+                  const valuePath = propertyPath.get('value');
 
-                if (
-                  keyPath.isIdentifier() &&
-                  (valuePath.isStringLiteral() || valuePath.isBooleanLiteral() || valuePath.isNumericLiteral())
-                ) {
-                  meta[keyPath.node.name] = valuePath.node.value;
+                  if (
+                    keyPath.isIdentifier() &&
+                    (valuePath.isStringLiteral() || valuePath.isBooleanLiteral() || valuePath.isNumericLiteral())
+                  ) {
+                    meta[keyPath.node.name] = valuePath.node.value;
+                  }
                 }
-              }
-            });
+              });
+            }
           }
-        }
-      },
-    });
+        },
+      });
 
-    /** @type {{ type: 'link'; label: string; href: string }} */
-    const sidebarItem = {
-      type: 'link',
-      label: meta.name,
-      href: `/try-it-out#${id}`,
-    };
+      /** @type {{ type: 'link'; label: string; href: string }} */
+      const sidebarItem = {
+        type: 'link',
+        label: meta.name,
+        href: `/try-it-out#${id}`,
+      };
 
-    sidebarItems[meta.position] = sidebarItem;
-  });
+      sidebarItems[meta.position] = sidebarItem;
 
-  return sidebarItems.filter(Boolean);
+      return sidebarItems;
+    }, [])
+    .filter(Boolean);
 }
 
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
