62cf39c860782a69ea9c89d797cdda094a3605a6
Oleksandr Fediashov <olfedias@microsoft.com>
2022-09-20 17:34:19 +0200

    docs: bump docusaurus & force dark theme (#215)

--- apps/website/src/theme/Navbar/ColorModeToggle/index.tsx
+++ apps/website/src/theme/Navbar/ColorModeToggle/index.tsx
@@ -1,37 +1,7 @@
 import * as React from 'react';
-import { useColorMode, useThemeConfig } from '@docusaurus/theme-common';
 
-import { GithubIcon, MoonIcon, SunIcon } from '../../../components/icons';
+import { GithubIcon } from '../../../components/icons';
 import styles from './styles.module.css';
-
-function ThemeToggle() {
-  const disabled = useThemeConfig().colorMode.disableSwitch;
-  const { colorMode, setColorMode } = useColorMode();
-
-  const isDarkMode = colorMode === 'dark';
-  const ariaLabel = isDarkMode ? 'Switch to light mode' : 'Switch to dark mode';
-  const icon = isDarkMode ? <SunIcon /> : <MoonIcon />;
-
-  const handleClick = React.useCallback(() => {
-    if (isDarkMode) {
-      setColorMode('light');
-    } else {
-      setColorMode('dark');
-    }
-  }, [isDarkMode, setColorMode]);
-
-  return (
-    <button
-      aria-label={ariaLabel}
-      className={styles['toggleButton']}
-      disabled={disabled}
-      onClick={handleClick}
-      type="button"
-    >
-      {icon}
-    </button>
-  );
-}
 
 function GithubLink() {
   return (
@@ -54,7 +24,6 @@ export default function () {
       {/* This is a hack to re-order Github link & Theme switcher. */}
       {/* The folder cannot be renamed as it's how swizzling works in Docusaurus: */}
       {/* https://docusaurus.io/docs/advanced/swizzling#swizzling-1 */}
-      <ThemeToggle />
       <GithubLink />
     </div>
   );
