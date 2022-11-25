9343f289bb0132f71491999700ba71533ea86d9f
Oleksandr Fediashov <olfedias@microsoft.com>
2022-11-25 15:53:46 +0100

    chore: bump monosize

---
+++ monosize.config.mjs
@@ -0,0 +1,25 @@
+import path from 'path';
+import upstashStorage from 'monosize-storage-upstash';
+
+const dirname = new URL('.', import.meta.url).pathname;
+console.log(dirname, path.resolve(dirname, './dist/packages/core/index.esm.js'));
+export default {
+  repository: 'https://github.com/microsoft/griffel',
+  storage: upstashStorage({
+    url: 'https://us1-nearby-hyena-35747.upstash.io',
+    readonlyToken:
+      'AoujASQgNGQwNGI0YjAtNmExNy00MzNjLWIwNTQtMTIyMGRlODQwZDk2QaCJNVtEfHj6pDLLscOcgMEt-Be1VWcMYbMRR_EqVCw=',
+  }),
+  webpack: config => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          '@griffel/core': path.resolve(dirname, './dist/packages/core/index.esm.js'),
+          '@griffel/react': path.resolve(dirname, './dist/packages/react/index.esm.js'),
+        },
+      },
+    };
+  },
+};
