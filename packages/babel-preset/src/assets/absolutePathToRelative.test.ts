f985b6dc5e3576f5aacce45752755d06cff7ef70
Oleksandr Fediashov <olfedias@microsoft.com>
2022-11-30 18:00:58 +0100

    fix: update path normalization in absolutePathToRelative (#284)

--- packages/babel-preset/src/assets/absolutePathToRelative.test.ts
+++ packages/babel-preset/src/assets/absolutePathToRelative.test.ts
@@ -20,6 +20,15 @@ describe('absolutePathToRelative', () => {
         'src/styles/Component.png',
       ),
     ).toBe('./Component.png');
+
+    expect(
+      absolutePathToRelative(
+        path.posix,
+        '/home/projects/foo',
+        '/home/projects/foo/packages/components/src/index.styles.ts',
+        'packages/components/src/images/Component.png',
+      ),
+    ).toBe('./images/Component.png');
   });
 
   it('handles Windows paths', () => {
@@ -40,5 +49,14 @@ describe('absolutePathToRelative', () => {
         'src/styles/Component.png',
       ),
     ).toBe('./Component.png');
+
+    expect(
+      absolutePathToRelative(
+        path.win32,
+        'C:\\Users\\Foo\\projects',
+        'C:\\Users\\Foo\\projects\\packages\\components\\src\\index.styles.ts',
+        'packages/components/src/images/Component.png',
+      ),
+    ).toBe('./images/Component.png');
   });
 });
