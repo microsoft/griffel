906dd5638e0823c01ab1071261a055b9d93c2d9a
Oleksandr Fediashov <olfedias@microsoft.com>
2022-11-30 15:42:15 +0100

    docs: update footer

--- apps/website/src/theme/Footer/index.tsx
+++ apps/website/src/theme/Footer/index.tsx
@@ -2,9 +2,6 @@ import { useThemeConfig } from '@docusaurus/theme-common';
 import FooterLinks from '@theme/Footer/Links';
 import * as React from 'react';
 
-import { TeamsIcon } from '../../components/icons';
-import styles from './index.module.css';
-
 function Footer() {
   const { footer } = useThemeConfig();
 
@@ -16,25 +13,7 @@ function Footer() {
 
   return (
     <div className="container margin-top--xl">
-      <div className="row row--no-gutters">
-        <div className="col">
-          <div className={styles['teamsLink']}>
-            <TeamsIcon />
-            <a
-              href="https://teams.microsoft.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="footer__link-item"
-            >
-              Created by Teams
-            </a>
-          </div>
-        </div>
-
-        <div className="col">
-          <FooterLinks links={links} />
-        </div>
-      </div>
+      <FooterLinks links={links} />
     </div>
   );
 }
