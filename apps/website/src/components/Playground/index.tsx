63b17936b4813a565d7872d906422f19c488fbc2
Oleksandr Fediashov <olfedias@microsoft.com>
2023-01-04 12:24:41 +0100

    chore: bump @codesandbox/sandpack-react (#302)

--- apps/website/src/components/Playground/index.tsx
+++ apps/website/src/components/Playground/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SandpackProvider, SandpackLayout, SandpackCodeEditor, SandpackPreview } from '@codesandbox/sandpack-react';
-import { useColorMode } from '@docusaurus/theme-common';
+import { oceanBlue } from '@codesandbox/sandpack-themes';
 import { useLocation } from '@docusaurus/router';
 
 import AppCode from './code/app';
@@ -21,35 +21,33 @@ const templates: Record<string, string> = ctx.keys().reduce((acc, modulePath) =>
 const PLAYGROUND_HEIGHT = 400;
 
 export default function Playground() {
-  const { colorMode } = useColorMode();
-
-  const sandpackTheme = colorMode === 'dark' ? 'dark' : 'github-light';
   const location = useLocation();
   const template = templates[location.hash.slice(1)] ?? DefaultStylesCode;
 
   return (
     <SandpackProvider
+      theme={oceanBlue}
       template="react"
+      files={{
+        '/App.js': {
+          // "AppCode" is a string as it's processed by "raw-loader", see "webpackLoader.js"
+          code: AppCode as unknown as string,
+          hidden: true,
+        },
+        // Template files are in JS but type checked, don't want unnecessary comments leaking into docs
+        '/styles.js': { code: template, active: true },
+      }}
       customSetup={{
         dependencies: {
           '@griffel/core': 'latest',
           'highlight.js': 'latest',
           'js-beautify': 'latest',
         },
-        files: {
-          '/App.js': {
-            // "AppCode" is a string as it's processed by "raw-loader", see "webpackLoader.js"
-            code: AppCode as unknown as string,
-            hidden: true,
-          },
-          // Template files are in JS but type checked, don't want unnecessary comments leaking into docs
-          '/styles.js': { code: template, active: true },
-        },
       }}
     >
-      <SandpackLayout theme={sandpackTheme}>
-        <SandpackCodeEditor showLineNumbers customStyle={{ height: PLAYGROUND_HEIGHT }} />
-        <SandpackPreview customStyle={{ height: PLAYGROUND_HEIGHT }} showOpenInCodeSandbox={false} />
+      <SandpackLayout>
+        <SandpackCodeEditor showLineNumbers style={{ height: PLAYGROUND_HEIGHT }} />
+        <SandpackPreview style={{ height: PLAYGROUND_HEIGHT }} showOpenInCodeSandbox={false} />
       </SandpackLayout>
     </SandpackProvider>
   );
