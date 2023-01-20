39b81b13428945e2dd523ac4832c4ed72021c531
Oleksandr Fediashov <olfedias@microsoft.com>
2023-01-20 14:48:19 +0100

    docs: add sharing feature to "Try it out"

--- apps/website/src/components/Playground/index.tsx
+++ apps/website/src/components/Playground/index.tsx
@@ -5,6 +5,8 @@ import { useLocation } from '@docusaurus/router';
 
 import AppCode from './code/app';
 import DefaultStylesCode from './code/styles';
+import { PlaygroundUrl } from './PlaygroundUrl';
+import { PlaygroundShare } from './PlaygroundShare';
 
 const ctx = require.context('./code/templates', false, /\.js$/);
 
@@ -25,30 +27,38 @@ export default function Playground() {
   const template = templates[location.hash.slice(1)] ?? DefaultStylesCode;
 
   return (
-    <SandpackProvider
-      theme={oceanBlue}
-      template="react"
-      files={{
-        '/App.js': {
-          // "AppCode" is a string as it's processed by "raw-loader", see "webpackLoader.js"
-          code: AppCode as unknown as string,
-          hidden: true,
-        },
-        // Template files are in JS but type checked, don't want unnecessary comments leaking into docs
-        '/styles.js': { code: template, active: true },
-      }}
-      customSetup={{
-        dependencies: {
-          '@griffel/core': 'latest',
-          'highlight.js': 'latest',
-          'js-beautify': 'latest',
-        },
-      }}
-    >
-      <SandpackLayout>
-        <SandpackCodeEditor showLineNumbers style={{ height: PLAYGROUND_HEIGHT }} />
-        <SandpackPreview style={{ height: PLAYGROUND_HEIGHT }} showOpenInCodeSandbox={false} />
-      </SandpackLayout>
-    </SandpackProvider>
+    <>
+      <SandpackProvider
+        theme={oceanBlue}
+        template="react"
+        files={{
+          '/App.js': {
+            // "AppCode" is a string as it's processed by "raw-loader", see "webpackLoader.js"
+            code: AppCode as unknown as string,
+            hidden: true,
+          },
+          // Template files are in JS but type checked, don't want unnecessary comments leaking into docs
+          '/styles.js': { code: template, active: true },
+        }}
+        customSetup={{
+          dependencies: {
+            '@griffel/core': 'latest',
+            'highlight.js': 'latest',
+            'js-beautify': 'latest',
+          },
+        }}
+      >
+        <SandpackLayout>
+          <SandpackCodeEditor showLineNumbers style={{ height: PLAYGROUND_HEIGHT }} />
+          <SandpackPreview style={{ height: PLAYGROUND_HEIGHT }} showOpenInCodeSandbox={false} />
+        </SandpackLayout>
+
+        <PlaygroundUrl template={template} />
+      </SandpackProvider>
+
+      <div className="margin-top--xs" style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <PlaygroundShare />
+      </div>
+    </>
   );
 }
