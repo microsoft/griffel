6099f8d16a796f50393eb51abe7b5d5fe62d6219
Michael Loughry <miclo@microsoft.com>
2023-08-09 16:56:34 +0000

    Add test case

---
+++ packages/webpack-loader/__fixtures__/webpack-resolve-options/code.ts
@@ -0,0 +1,8 @@
+import { makeStyles } from '@griffel/react';
+import color from './color';
+
+export const styles = makeStyles({
+  root: {
+    backgroundColor: color,
+  },
+});
---
+++ packages/webpack-loader/__fixtures__/webpack-resolve-options/output.ts
@@ -0,0 +1,12 @@
+import { __styles } from '@griffel/react';
+import color from './color';
+export const styles = __styles(
+  {
+    root: {
+      De3pzq: 'f1bh81bl',
+    },
+  },
+  {
+    d: ['.f1bh81bl{background-color:blue;}'],
+  },
+);
