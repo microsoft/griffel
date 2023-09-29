49c1a7cd276b62bd4a6237604c21dee87d567dd3
Oleksandr Fediashov <olfedias@microsoft.com>
2023-09-29 10:56:35 +0200

    chore: remove unstable-css-lint rule (#446)

--- packages/eslint-plugin/src/rules/css-lint/.stylelintrc-test.js
+++
@@ -1,9 +0,0 @@
-/**
- * A test stylelint config for tests
- */
-
-module.exports = {
-  rules: {
-    'selector-anb-no-unmatchable': [true],
-  },
-};
--- packages/eslint-plugin/src/rules/css-lint/types.ts
+++
@@ -1,6 +0,0 @@
-import type { Warning } from 'stylelint';
-
-export interface StylelintSyncReturn {
-  error?: string;
-  stylelintErrors: Warning[];
-}
