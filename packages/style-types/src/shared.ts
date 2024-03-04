2e24849b0bc07ccde9eb7fb1bbb0b3cdd898942b
Oleksandr Fediashov <olfedias@microsoft.com>
2024-03-04 13:26:52 +0100

    Update packages/style-types/src/shared.ts
    Co-authored-by: Martin Hochel <hochelmartin@gmail.com>

--- packages/style-types/src/shared.ts
+++ packages/style-types/src/shared.ts
@@ -1,4 +1,8 @@
 export type GriffelStylesCSSValue = string | 0;
 
 export type ValueOrArray<T> = T | Array<T>;
+/**
+ * instead of using `CSS.PropertiesFallback` from csstype that remaps properties to `readonly` union we need to use our cutom Fallback mapped type in order to avoid type issues
+ * @see https://github.com/frenic/csstype/blob/master/src/typescript.ts#L73
+ */
 export type Fallback<T> = { [P in keyof T]: T[P] | NonNullable<T[P]>[] };
