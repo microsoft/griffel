346e7246c24f068e97d115d8f897da825c26820d
Oleksandr Fediashov <olfedias@microsoft.com>
2024-04-23 18:11:06 +0200

    chore: add package for a script for shorthand updates (#547)

---
+++ tools/update-shorthands/jest.config.ts
@@ -0,0 +1,17 @@
+/* eslint-disable */
+export default {
+  displayName: 'update-shorthands',
+  preset: '../../jest.preset.js',
+  globals: {},
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  coverageDirectory: '../../coverage/tools/update-shorthands',
+};
