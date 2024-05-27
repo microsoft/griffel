26dd4c256dd8ecc6e2b72a73e38f7831c4cfad96
Oleksandr Fediashov <olfedias@microsoft.com>
2024-05-27 14:08:44 +0200

    fix(tar-processor): fix handling of meta (#564)
    * fix(tar-processor): fix handling of meta
    
    * Change files

--- packages/vite-plugin/__fixtures__/object/output.ts
+++ packages/vite-plugin/__fixtures__/object/output.ts
@@ -3,10 +3,23 @@ const styles = /* @__PURE__ */ __styles(
   {
     root: {
       sj55zd: 'fl9q5hc',
+      Byoj8tv: 0,
+      uwmqm3: 0,
+      z189sj: 0,
+      z8tnut: 0,
+      B0ocmuz: 'fbhmu18',
     },
   },
   {
-    d: ['.fl9q5hc{color:var(--colorBrandStroke1);}'],
+    d: [
+      '.fl9q5hc{color:var(--colorBrandStroke1);}',
+      [
+        '.fbhmu18{padding:10px;}',
+        {
+          p: -1,
+        },
+      ],
+    ],
   },
 );
 console.log(styles);
