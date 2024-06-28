6b885b43e35a732896c4e01834bf19b63a26252a
Oleksandr Fediashov <olfedias@microsoft.com>
2024-06-28 11:06:17 -0700

    chore: bump Nx to 18.3.5

--- packages/webpack-extraction-plugin/__fixtures__/babel/reset/output.ts
+++ packages/webpack-extraction-plugin/__fixtures__/babel/reset/output.ts
@@ -1,3 +1,2 @@
-import { __resetCSS as _resetCSS } from '@griffel/react';
-import { __resetStyles } from '@griffel/react';
+import { __resetStyles, __resetCSS as _resetCSS } from '@griffel/react';
 export const useClassName = _resetCSS('rjefjbm', 'r7z97ji');
--- tools/getRollupOptions.js
+++ tools/getRollupOptions.js
@@ -1,18 +1,18 @@
 function getRollupOptions(/** @type {import('rollup').RollupOptions} */ options) {
   if (Array.isArray(options.output)) {
-    throw new Error('"options.output" cannot be an array');
+    options.output = options.output.map(output => ({
+      ...output,
+      // Stops bundling to a single file and prevents bundle size issues
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+      // Enables sourcemaps
+      sourcemap: true,
+    }));
+
+    return options;
   }
 
-  options.output = {
-    ...options.output,
-    // Stops bundling to a single file and prevents bundle size issues
-    preserveModules: true,
-    preserveModulesRoot: 'src',
-    // Enables sourcemaps
-    sourcemap: true,
-  };
-
-  return options;
+  throw new Error('"options.output" should be an array');
 }
 
 module.exports = getRollupOptions;
