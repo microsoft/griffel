fb21c433253790424a07159ede50c9f827b51137
Sean Monahan <seanmonahan@microsoft.com>
2024-08-08 02:01:21 -0700

    fix: remove eslint exception in `RendererProvider` (#566)
    * fix: make RendererProvider isomorphic without disabling lint rules
    
    Moves the `canUseDOM()` call inside the `useMemo()` hook, allowing
    RendererProvider to work without disabling React hooks linting and
    therefore potential React Compiler optimizations.
    
    * Change files
    
    ---------
    
    Co-authored-by: Oleksandr Fediashov <olfedias@microsoft.com>

--- packages/react/src/RendererContext.tsx
+++ packages/react/src/RendererContext.tsx
@@ -28,18 +28,13 @@ const RendererContext = React.createContext<GriffelRenderer>(createDOMRenderer()
  * @public
  */
 export const RendererProvider: React.FC<RendererProviderProps> = ({ children, renderer, targetDocument }) => {
-  'use no memo';
-
-  if (canUseDOM()) {
-    // This if statement technically breaks the rules of hooks, but is safe because the condition never changes after
-    // mounting.
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    React.useMemo(() => {
+  React.useMemo(() => {
+    if (canUseDOM()) {
       // "rehydrateCache()" can't be called in effects as it needs to be called before any component will be rendered to
       // avoid double insertion of classes
       rehydrateRendererCache(renderer, targetDocument);
-    }, [renderer, targetDocument]);
-  }
+    }
+  }, [renderer, targetDocument]);
 
   return <RendererContext.Provider value={renderer}>{children}</RendererContext.Provider>;
 };
