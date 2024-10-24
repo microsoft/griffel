fc071ce66b26908ca09e05fd5c8ef0dd409372de
Oleksandr Fediashov <olfedias@microsoft.com>
2024-10-24 11:46:27 +0200

    chore: bump Nx to 19.8.6

--- packages/shadow-dom/.storybook/main.js
+++ packages/shadow-dom/.storybook/main.js
@@ -2,14 +2,14 @@ const rootMain = require('../../../.storybook/main');
 
 module.exports = {
   ...rootMain,
-  core: { ...rootMain.core, builder: 'webpack5' },
 
+  core: { ...rootMain.core, builder: 'webpack5' },
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
   },
 
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(ts|tsx)'],
+  stories: [...rootMain.stories, '../src/**/*.stories.mdx', '../src/**/*.stories.@(ts|tsx)'],
   addons: ['@storybook/addon-essentials', ...rootMain.addons, '@nx/react/plugins/storybook'],
 
   webpackFinal: async (config, { configType }) => {
@@ -21,9 +21,5 @@ module.exports = {
     // add your own webpack tweaks if needed
 
     return config;
-  },
-
-  typescript: {
-    reactDocgen: 'react-docgen-typescript',
   },
 };
