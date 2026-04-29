910d4e462e7b4de15c83eaeaf76987b6d7f13283
Oleksandr Fediashov <olfedias@microsoft.com>
2026-04-29 18:49:02 +0200

    fixup: keep default React imports in @internal helpers
    `import * as React` adds webpack's namespace runtime helper to the
    bundle (~1KB on `__styles`). These two files are `@internal` and
    stripped from the published `.d.ts`, so a default import is fine —
    suppress TS1259 in spec/storybook contexts with `@ts-ignore`.

--- packages/react/src/useInsertionEffect.ts
+++ packages/react/src/useInsertionEffect.ts
@@ -1,11 +1,12 @@
 'use client';
 
-import * as React from 'react';
+// @ts-ignore — default import is intentional (avoids the webpack namespace helper that `import * as` adds); this file is `@internal` and is stripped from the published `.d.ts`.
+import React from 'react';
 
 /**
  * @internal
  */
 export const useInsertionEffect: typeof React.useInsertionEffect | undefined =
-  // @ts-expect-error Hack to make sure that `useInsertionEffect` will not cause bundling issues in older React versions
+  // @ts-ignore — Hack to make sure that `useInsertionEffect` will not cause bundling issues in older React versions
   // eslint-disable-next-line no-useless-concat
   React['useInsertion' + 'Effect'] ? React['useInsertion' + 'Effect'] : undefined;
--- packages/react/src/utils/isInsideComponent.ts
+++ packages/react/src/utils/isInsideComponent.ts
@@ -1,6 +1,7 @@
 'use client';
 
-import * as React from 'react';
+// @ts-ignore — default import is intentional (avoids the webpack namespace helper that `import * as` adds); this file is `@internal` and is stripped from the published `.d.ts`.
+import React from 'react';
 
 function getDispatcher() {
   try {
